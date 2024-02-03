b51feb3ebda1726f1bf6a619798ca019374a35a6
yashsomkuwar <yash@getswipe.in>
2024-02-03 15:02:51 +0530

    Initial Commit

---
+++ AssignmentFamPay/AssignmentFamPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentFamPayApp.swift
+//  AssignmentFamPay
+//
+//  Created by Yash Somkuwar on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentFamPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
